9a38b9bc6d240b5b6721325e7a96bfb3d9e6fcaa
Mateusz <mmaciag922@gmail.com>
2021-12-09 00:08:32 +0100

    init project

---
+++ src/test/java/pl/wsiz/restaurantservice/RestaurantserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsiz.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
